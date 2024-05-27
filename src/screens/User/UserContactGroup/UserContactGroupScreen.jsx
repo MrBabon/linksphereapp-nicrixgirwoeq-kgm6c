948be6cda1a1548fa7f8da5db8c0219aa43402ea
MrBabon <christophedanna06@gmail.com>
2024-05-27 21:31:37 +0200

    Usercontactgroup en cours

--- src/screens/User/UserContactGroup/UserContactGroupScreen.jsx
+++ src/screens/User/UserContactGroup/UserContactGroupScreen.jsx
@@ -1,13 +1,44 @@
-import { View } from "react-native";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 import { TxtInria } from "../../../components/TxtInria/TxtInria";
 import { s } from "./UserContactGroupScreen.style";
+import ChevronLeft from "../../../assets/icons/ChevronLeft";
+import { TxtJost } from "../../../components/TxtJost/TxtJost";
+import Spinner from "react-native-loading-spinner-overlay";
+import Send from "../../../assets/icons/Send";
 
 const UserContactGroupScreen = ({ navigation }) => {
+    const BackButton = (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+            <ChevronLeft/>
+        </TouchableOpacity>
+    )
+
+    const Delete = (
+        <TouchableOpacity>
+            <Send/>
+        </TouchableOpacity>
+    )
+
+    const header = (
+        <View style={s.container_header}>
+            <View style={s.header}>
+                {BackButton}
+                <View style={s.header_texts}>
+                    <TxtJost style={s.txtheader}>Contact</TxtJost>
+                </View>
+                {Delete}
+            </View>
+        </View>
+    )
 
     return (
-        <View>
-            <TxtInria>USERCONTACTGROUP</TxtInria>
-        </View>
+        <>
+            <Spinner/>
+            {header}
+            <ScrollView>
+                <TxtInria>USERCONTACTGROUP</TxtInria>
+            </ScrollView>
+        </>
     )
 }
 
--- src/screens/User/UserContactGroup/UserContactGroupScreen.style.js
+++ src/screens/User/UserContactGroup/UserContactGroupScreen.style.js
@@ -1,4 +1,36 @@
 import { StyleSheet } from "react-native";
 
 
-export const s = StyleSheet.create({})
+export const s = StyleSheet.create({
+
+    // HEADER
+
+    container_header: {
+        zIndex: 1
+    },
+
+    header: {
+        flexDirection: "row",
+        position: "absolute",
+        width: "100%",
+        backgroundColor: "#1AC1B9",
+        height: 110,
+        paddingTop: 60,
+        paddingHorizontal: 14,
+        zIndex: 3
+    },
+
+    header_texts: {
+        flex: 1,
+        alignItems: "center",
+    },
+
+
+
+    txtheader: {
+        fontFamily: "Jost-bold",
+        textTransform: "uppercase",
+        fontSize: 20,
+        color: "#F4F4F4"
+    },
+})
